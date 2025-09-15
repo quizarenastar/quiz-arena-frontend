0206fffa1dda15fab8220ddba02eeffdb561fa24
mohdrafey1 <mohdrafey600@gmail.com>
2025-09-15 15:22:08 +0530

    Uncomment Home route in RoutesComponent for proper navigation

--- src/RoutesComponent.jsx
+++ src/RoutesComponent.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
-// import Home from './Pages/Home';
+import Home from './Pages/Home';
 import Login from './Pages/Login';
 import Signup from './Pages/Signup';
 
 export default function RoutesComponent() {
     return (
         <Routes>
-            {/* <Route path='/' element={<Home />} /> */}
+            <Route path='/' element={<Home />} />
             <Route path='/login' element={<Login />} />
             <Route path='/signup' element={<Signup />} />
         </Routes>
